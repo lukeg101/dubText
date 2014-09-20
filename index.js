8093db5bca6681e4f2e12d2352f6da995d028bcd
lukeg101 <lukegeeson@googlemail.com>
2014-09-20 18:29:16 +0100

    server stuff

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ var express = require('express'),
 	app = express(),
 	client = twilio('AC37cb5af509c24ae9dbe5c01e48d1f412','048f0c1de0b8c3978539dac94ac0fe8d');
 
+app.get('/', function(req, res){
+	res.end('end');
+})
+
 
 app.get('/start',function(req, res){
 	console.log('get request received');
-
+	console.log(req);
 	var resp = new twilio.TwimlResponse();
-
 });
 
 app.listen(process.env.PORT || 80);
